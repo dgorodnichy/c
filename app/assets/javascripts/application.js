1ce3bc35fdbc83ab13bbc1eb72d6242b5a9b52bb
Dmitry Gorodnichy <gorodni4ij@gmail.com>
2018-07-30 00:17:08 +0300

    add materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require materialize.min.js
 //= require_tree .
--- app/javascript/controllers/movie_controller.js
+++ app/javascript/controllers/movie_controller.js
@@ -6,19 +6,22 @@ export default class extends Controller {
   static targets = [ "output", "name", 'submit', 'spinner' ]
 
   connect() {
-    // this.outputTarget.textContent = 'Hello, Stimulus!'
+    console.log('Hello, Stimulus!')
   }
 
   greet() {
+    event.preventDefault()
     var context = this
 
-    context.spinnerTarget.style.display = 'block';
+    context.spinnerTarget.classList.remove("hide");
+    context.outputTarget.classList.add("hide");
 
     axios.get('/', {
       params: { query: this.nameTarget.value }
     }).then(function (response) {
       context.outputTarget.innerHTML = response.data
-      context.spinnerTarget.style.display = 'none';
+      context.spinnerTarget.classList.add("hide");
+      context.outputTarget.classList.remove("hide");
     })
   }
 }
